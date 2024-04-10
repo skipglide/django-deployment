da181dbc48acd056ff615e4535ed7793654acac1
skipglide <gravesdavid133@gmail.com>
2024-04-10 14:32:46 -0500

    tatatatata

--- dploy/settings/base.py
+++ dploy/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(Path(__file__).resolve().parent.parent)
 
 
 # Application definition
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'dploy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
