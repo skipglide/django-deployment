2795a99120424ef6d9bf5ddda444c30803d99a6a
skipglide <gravesdavid133@gmail.com>
2024-04-11 21:09:20 -0500

    a

--- dploy/settings/dev.py
+++ dploy/settings/dev.py
@@ -2,4 +2,4 @@ from .base import *
 
 SECRET_KEY = 'django-insecure-61#!aafnxymcx&&z5zgwfgqo^&igfo&j5f&2!isnnc7o2f0k@f'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','45.55.40.138']
