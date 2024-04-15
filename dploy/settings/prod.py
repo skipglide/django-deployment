4b626a027e7d83140481cd76f7feee6cb1ae2c93
skipglide <gravesdavid133@gmail.com>
2024-04-15 14:16:13 -0500

    added 0.0.0.0

--- dploy/settings/prod.py
+++ dploy/settings/prod.py
@@ -3,4 +3,4 @@ from .base import *
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1','localhost','45.55.40.138']
+ALLOWED_HOSTS = ['0.0.0.0','45.55.40.138']
