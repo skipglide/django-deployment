878e82c4b14acacd16d3eea4ec681293177414dc
skipglide <gravesdavid133@gmail.com>
2024-04-19 15:12:46 -0500

    Added Post

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 class Counter(models.Model):
     count = models.IntegerField(default=0)
+
+class Post(models.Model):
+        image = models.FileField(upload_to='', validators=[FileExtensionValidator(['jpg', 'png', 'webp', 'mp4', 'webm'], 'Not a common image or video format.')], null=True)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,50 @@
-from django.shortcuts import render
+import os
 
-from .models import Counter
+from django.conf import settings
+from django.shortcuts import render, redirect
+
+from .forms import PostForm
+from .models import Counter, Post
 
 def home(request):
+    post = Post.objects.first()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Delete previous images if any
+            clear_media()
+            Post.objects.all().delete()
+            # Save the new image
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, "app/home.html", {"post": post, "form": form})
+    else:
+        form = PostForm()
+    
     counter = Counter.objects.last()
     if not counter:
         counter = Counter.objects.create()
     
     counter.count += 1
     counter.save()
-    counter = {
+    context = {
         "count": counter.count,
+        "post": post,
+        "form": form
     }
-    return render(request, "app/home.html", counter)
+    return render(request, "app/home.html", context)
+
+def clear_media():
+    media_folder = settings.MEDIA_ROOT
+    if os.path.exists(media_folder):
+        # Delete all files in the folder
+        for filename in os.listdir(media_folder):
+            file_path = os.path.join(media_folder, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Error deleting {file_path}: {e}")
