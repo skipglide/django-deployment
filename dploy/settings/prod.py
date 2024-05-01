57244addff8544c897099311dff6f5c12c7fb620
skipglide <154386434+skipglide@users.noreply.github.com>
2024-05-01 06:46:19 -0500

    Update prod.py

--- dploy/settings/prod.py
+++ dploy/settings/prod.py
@@ -4,5 +4,4 @@ from .base import *
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = False
 ALLOWED_HOSTS = ['0.0.0.0', 'janus-vault.com', 'www.janus-vault.com']
-SECURE_SSL_REDIRECT = True
 CSRF_TRUSTED_ORIGINS = ["https://janus-vault.com", "https://www.janus-vault.com"]
